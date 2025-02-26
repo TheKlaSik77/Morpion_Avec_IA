39ed9f2555c191a42e671a1f058aa0928cd09252
TheKlaSik77 <kylian77280@gmail.com>
2025-02-27 00:46:52 +0100

    Ajout set_grille dans grille.py et des tests unitaires dans test_grille.py

--- test/test_grille.py
+++ test/test_grille.py
@@ -1,4 +1,4 @@
-from models import Grille
+from modele.grille import Grille
 import unittest
 
 class TestGrille(unittest.TestCase):
@@ -13,6 +13,31 @@ class TestGrille(unittest.TestCase):
                        [0,0,0]
                        ]
         self.assertEqual(grille.grille, grille_test)
+
+    def tester_set_grille(self):
+        grille = Grille()
+        nouvelle_grille = [
+            [1,0,0],
+            [0,1,0],
+            [2,0,0]
+        ]
+        grille.set_grille(nouvelle_grille)
+        self.assertEqual(grille.grille,nouvelle_grille)
+
+    def tester_poser_coup_valide(self):
+        grille = Grille()
+        grille_test = [
+            [1,0,0],
+            [0,1,0],
+            [2,0,0]
+        ]
+        resultat_bon = grille.poser_coup(0,1,2)
+        self.assertTrue(resultat_bon)
+        self.assertEqual(grille.grille[0][1],2)
+        resultat_echec = grille.poser_coup(0,1,1)
+        self.assertFalse(resultat_echec)
+        self.assertEqual(grille.grille[0][1], 2)
+
 
 if __name__ == "__main__":
     unittest.main()
